ef4faaf92b72d82964c9a2267999f6ad2598b018
GiharaNavindu <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2025-05-28 15:53:39 +0530

    react query-form

--- Intern/Quarkus/frontend/vite-project/src/App.tsx
+++ Intern/Quarkus/frontend/vite-project/src/App.tsx
@@ -15,8 +15,8 @@ import StudentEnrollments from "./pages/StudentEnrollment";
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
-  const [role, setRole] = useState<string>("Student"); 
-  const [userId, setUserId] = useState<string>("STU002");
+  const [role, setRole] = useState<string>("Lecturer"); 
+  const [userId, setUserId] = useState<string>("LEC001");
 
   return (
     <QueryClientProvider client={queryClient}>
